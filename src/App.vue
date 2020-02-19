15326b3728c969021a0a3fbc6335dd7961a9498f
wemali05 <alfredwemali05@gmail.com>
2020-02-19 17:48:24 +0300

    transition-group, animate group

--- src/App.vue
+++ src/App.vue
@@ -59,7 +59,12 @@
                 <button class="btn btn-primary" @click="addNumber">Add Number</button>
                 <br><br>
                 <ul class="list-group">
-                    <li class="list-group-item" v-for="(number, index) in numbers" @click="removeNumber" style="cursor:pointer">{{ number }}</li>
+                 <transition-group name="slide">
+                    <li class="list-group-item" v-for="(number, index) in numbers" 
+                     @click="removeNumber"
+                     style="cursor:pointer"
+                     :key="number">{{ number }}</li>
+                 </transition-group>   
                 </ul>
 
 
@@ -177,6 +182,11 @@ import SuccessAlert from './SuccessAlert.vue';
         animation:  slide-out 1s ease-out forwards;
          transition: opacity 1s;
          opacity: 0;
+         position: absolute;
+    }
+
+    .slide-move{
+        transition: transform 1s;
     }
 
     @keyframes slide-in{
