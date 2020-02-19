3c5749943d8b7945b6f62921ef8b3584b60e5856
wemali05 <alfredwemali05@gmail.com>
2020-02-19 15:34:58 +0300

    javascript animation

--- src/App.vue
+++ src/App.vue
@@ -27,6 +27,22 @@
                    <div class="alert alert-info"  key="info" v-if="show">This is some Info </div>
                    <div class="alert alert-warning"  key="warning" v-else>This is some Info </div>
                 </transition>
+
+                <hr>
+                <button class="btn btn-primary" @click="load = !load">Load/ Remove Elemnt</button>
+                <br><br>
+                <transition
+                    @before-enter="beforeEnter"
+                    @enter="enter"
+                    @after-enter="afterEnter"
+                    @enter-cancelled="enterCancelled"
+                
+                    @before-leave="beforeLeave"
+                    @leave="leave"
+                    @after-leave="afterLeave"
+                    @leave-cancelled="leaveCancelled">
+                    <div style="height:100px; width:100px; background-color:lightgreen;" v-if="load"></div>
+                </transition>
             </div>
         </div>
     </div>
@@ -36,10 +52,40 @@
     export default {
         data() {
             return {
-                show: true,
+                show: false,
+                load: true,
                 alertAnimation: 'fade'
             }
-        }
+        },
+
+        methods: {
+            beforeEnter(el){
+                console.log('beforeEnter');
+            },
+            enter(el, done){
+                console.log('enter');
+                done();
+            },
+            afterEnter(el){
+                 console.log('afterEnter');
+            },
+            enterCancelled(el){
+                 console.log('enterCancelled');
+            },
+            beforeLeave(el){
+                  console.log('beforeLeave');
+            },
+            leave(el, done){
+                console.log('leave');
+                done();
+            },
+            afterLeave(el){
+                  console.log('afterLeave');
+            },
+            leaveCancelled(el){
+                console.log('leaveCancelled');
+            }
+        },
     }
 </script>
 
