459a33661f73eaa26a3fe33063512328fe41135a
wemali05 <alfredwemali05@gmail.com>
2020-02-19 12:20:35 +0300

    assign css classes for transition

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
                 <hr>
                 <button class=" btn btn-primary" @click="show=!show">Show Alert</button>
                 <br><br>
-                <transition>
+                <transition name="fade"> 
                   <div class="alert alert-info" v-if="show">This is some Info </div>
                 </transition>
                
@@ -26,5 +26,8 @@
 </script>
 
 <style>
-
+    .fade-enter{}
+    .fade-enter-active{}
+    .fade-leave{}
+    .fade-leave-active{}
 </style>
