d98d9df5c9a0439d03d38f313ed17fe7299cc45c
wemali05 <alfredwemali05@gmail.com>
2020-02-19 13:37:30 +0300

    dynamic names and attributes

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,20 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations</h1>
                 <hr>
+               <select v-model="alertAnimation" class="form-control"> 
+                    <option value="fade">fade</option>
+                    <option value="slide">slide</option>
+               </select>
+                <br><br>
                 <button class=" btn btn-primary" @click="show=!show">Show Alert</button>
                 <br><br>
-                <transition name="fade"> 
+                <transition :name="alertAnimation"> 
                   <div class="alert alert-info" v-show="show">This is some Info </div>
                 </transition>
-                <transition name="slide" type="animation"> 
+                <transition name="slide" type="animation" appear> 
                   <div class="alert alert-info" v-if="show">This is some Info </div>
                 </transition>
-                 <transition 
-                    appear
+                 <transition
                     enter-active-class="animated bounce"
                     leave-active-class="animated shake"
                     > 
@@ -28,7 +32,8 @@
     export default {
         data() {
             return {
-                show: true
+                show: true,
+                alertAnimation: 'fade'
             }
         }
     }
