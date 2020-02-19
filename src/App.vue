2be36e225f39ca49561a7c6b9be2bd542e2a7d02
wemali05 <alfredwemali05@gmail.com>
2020-02-19 12:41:35 +0300

    slide transition with the CSS animation property

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,9 @@
                   <div class="alert alert-info" v-if="show">This is some Info </div>
                 </transition>
                
+                 <transition name="slide"> 
+                  <div class="alert alert-info" v-if="show">This is some Info </div>
+                </transition>
             </div>
         </div>
     </div>
@@ -38,5 +41,36 @@
     .fade-leave-active{
         transition: opacity 1s;
         opacity: 0;
+    }
+
+    .slide-enter{
+     /* transform: translateY(20px);*/
+    }
+    .slide-enter-active{
+        animation:  slide-in 1s ease-out forwards;
+    }
+    .slide-leave{
+
+    }
+    .slide-leave-active{
+        animation:  slide-out 1s ease-out forwards;
+    }
+
+    @keyframes slide-in{
+       from {
+             transform: translateY(20px);
+       }
+       to {
+             transform: translateY(0);
+       }
+    }
+
+    @keyframes slide-out {
+        from{
+             transform: translateY(0);
+        }
+        to {
+               transform: translateY(20px);
+        }
     }
 </style>
